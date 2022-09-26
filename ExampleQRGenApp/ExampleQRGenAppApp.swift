182d4800f9e50cb1ec6a0a15b6c26557a264a896
Uliana Gritchina <ugritchina@icloud.com>
2022-09-26 18:46:22 +0300

    Initial Commit

---
+++ ExampleQRGenApp/ExampleQRGenAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleQRGenAppApp.swift
+//  ExampleQRGenApp
+//
+//  Created by Ульяна Гритчина on 26.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct ExampleQRGenAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
